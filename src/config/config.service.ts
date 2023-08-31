282aca32d9f6d5fcd7f8de7f3bddbb1ac716ac3a
qpd0ne <ts.google@yandex.ru>
2023-08-31 14:50:03 +0500

    create config

---
+++ src/config/config.service.ts
@@ -0,0 +1,24 @@
+import { DotenvParseOutput, config } from "dotenv";
+import { IConfigService } from "./config.interface";
+
+export class ConfigService implements IConfigService {
+  private config: DotenvParseOutput;
+  constructor() {
+    const {error, parsed} = config();
+    if (error) {
+        throw new Error('***not found .env***');
+    }
+    if (!parsed) {
+        throw new Error('***.env is empty***');
+    }
+    this.config = parsed;
+  }
+
+  get(key: string): string {
+    const res = this.config[key];
+    if (!key) {
+        throw new Error('***key is not found***');
+    }
+    return res;
+  }
+}
