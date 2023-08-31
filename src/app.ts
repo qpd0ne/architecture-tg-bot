99e5e5da716527bacb61cd8265a5f25201c157bb
qpd0ne <ts.google@yandex.ru>
2023-08-31 15:04:54 +0500

    create session in app.ts

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { Telegraf } from "telegraf";
+import { IConfigService } from "./config/config.interface";
+import { ConfigService } from "./config/config.service";
+
+class Bot {
+  bot: Telegraf<any>;
+
+  constructor(private readonly configService: IConfigService) {
+    this.bot = new Telegraf<any>(this.configService.get("NOKEN"));
+    this.bot.use(session());
+  }
+
+  init() {
+    this.bot.launch();
+  }
+}
+
+const bot = new Bot(new ConfigService());
+bot.init();
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -4,12 +4,12 @@ import { IConfigService } from "./config.interface";
 export class ConfigService implements IConfigService {
   private config: DotenvParseOutput;
   constructor() {
-    const {error, parsed} = config();
+    const { error, parsed } = config();
     if (error) {
-        throw new Error('***not found .env***');
+      throw new Error("***not found .env***");
     }
     if (!parsed) {
-        throw new Error('***.env is empty***');
+      throw new Error("***.env is empty***");
     }
     this.config = parsed;
   }
@@ -17,7 +17,7 @@ export class ConfigService implements IConfigService {
   get(key: string): string {
     const res = this.config[key];
     if (!key) {
-        throw new Error('***key is not found***');
+      throw new Error("***key is not found***");
     }
     return res;
   }
